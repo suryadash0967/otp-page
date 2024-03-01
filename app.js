6188e1d322dc266b286265a6b941f2416ff20691
suryadash0967 <surya.dash0967@gmail.com>
2024-03-02 00:34:59 +0530

    app.js updated

--- app.js
+++ app.js
@@ -6,25 +6,26 @@ let form = document.querySelector("form");
 
 inputs.forEach((input) => {
     input.addEventListener("keyup", (e) => {
-        let currentInput = input;
-        let nextInput = input.nextElementSibling;
-        let prevInput = input.previousElementSibling;
+        if (e.key >= 1 && e.key <= 9) {
+            let currentInput = input;
+            let nextInput = input.nextElementSibling;
+            let prevInput = input.previousElementSibling;
 
-        if (currentInput.value.length > 1) {
-            currentInput.value = "";
-        }
+            if (currentInput.value.length > 1) {
+                currentInput.value = "";
+            }
 
-        if (nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== "") {
-            nextInput.removeAttribute("disabled");
-            nextInput.focus();
-        }
-        if (e.key == "Backspace" && prevInput) {
-            prevInput.focus();
-            currentInput.setAttribute("disbled", true);
-        }
+            if (nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== "") {
+                nextInput.removeAttribute("disabled");
+                nextInput.focus();
+            }
+            if ((e.key == "Backspace" || e.key == "ArrowLeft") && prevInput) {
+                prevInput.focus();
+                currentInput.setAttribute("disabled", true);
+            }
 
-        
-    })
+        }
+        });
 });
 
 
